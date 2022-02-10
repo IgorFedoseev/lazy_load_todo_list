5e40af345efb68a4c69746a2c24354316e5e4f6c
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-02-10 19:40:26 +0300

    todo list init

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:lazyload_todo_list/widgets/app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
---
+++ lib/widgets/example/example_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ExampleWidget extends StatelessWidget {
+  const ExampleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(child: Text('HI'),),
+      ),
+    );
+  }
+}
