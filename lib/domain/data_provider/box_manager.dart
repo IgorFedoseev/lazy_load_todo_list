ffc8e41253b43e7cca9544684c5f5bd836a9f5cd
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-02-14 17:07:00 +0300

    added box manager

---
+++ lib/domain/data_provider/box_manager.dart
@@ -0,0 +1,30 @@
+import 'package:hive/hive.dart';
+import 'package:lazyload_todo_list/domain/entity/group.dart';
+import 'package:lazyload_todo_list/domain/entity/task.dart';
+
+class BoxManager {
+  Future<Box<Group>> openGroupBox() async {
+    return _openBox<Group>('groups_box', 1, GroupAdapter());
+  }
+
+  Future<Box<Task>> openTaskBox() async {
+    return _openBox<Task>('tasks_box', 2, TaskAdapter());
+  }
+
+  Future<void> closeBox<T>(Box<T> box) async{
+    await box.compact();
+    await box.close();
+  }
+
+  Future<Box<T>> _openBox<T>(
+    String name,
+    int typeId,
+    TypeAdapter<T> adapter,
+  ) async {
+    if (!Hive.isAdapterRegistered(typeId)) {
+      Hive.registerAdapter(adapter);
+    }
+    final box = await Hive.openBox<T>(name);
+    return box;
+  }
+}
